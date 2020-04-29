7ef87d223142344662fceda7dc8967ea54258148
Jose Antonio Bautista <jbautistam@gmail.com>
2020-04-29 17:29:55 +0200

    Error en la lectura de csv

---
+++ src/Apps/BauDbStudio/Views/Log/LogView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+
+using Bau.Libraries.DbStudio.ViewModels.Tools;
+
+namespace Bau.DbStudio.Views.Log
+{
+	/// <summary>
+	///		Vista para el log
+	/// </summary>
+	public partial class LogView : UserControl
+	{
+		public LogView(LogListViewModel viewModel)
+		{
+			InitializeComponent();
+			DataContext = ViewModel = viewModel;
+		}
+
+		/// <summary>
+		///		ViewModel de log
+		/// </summary>
+		public LogListViewModel ViewModel { get; }
+
+		private void ListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			if ((sender as ListView)?.SelectedItem is LogListItemViewModel logItem)
+			{
+				logItem.ShowDetailsCommand.Execute(null);
+			}
+		}
+	}
+}
