8b2c618c49da780b38535bf495c3c7b24ddbc89f
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2024-01-13 11:41:59 +0100

    Tests

--- src/Libraries/DbReporting/Tests/LibReporting.Tests/LibReporting.Tests/Data/ReportingRoi/Curves/NoDimensions.response.2.sql
+++
@@ -1,8 +0,0 @@
-SELECT Curves.[Week] AS [Week],
-						   CAST(AVG(Curves.[Value]) AS decimal(10,2)) AS [Percentage]                            
-                     FROM Fact.CurvesByPointOfSaleAndProduct INNER JOIN Fact.Curves
-	                    ON CurvesByPointOfSaleAndProduct.CurveId = Curves.CurveId                                   
-						
-		            	
-		            GROUP BY Curves.[Week]
-		            ORDER BY Curves.[Week] ASC
